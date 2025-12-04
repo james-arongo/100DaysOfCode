739406e0ecd1d1d505d21a50a199c6610e0f96c4
James Arong'o <git.drinkable312@passfwd.com>
2025-12-04 15:06:08 +0300

    Creates band name generator program

---
+++ python/Day_01/08-day1-project.py
@@ -0,0 +1,5 @@
+# This a CLI program that creates a band name by combining answers from two questions
+print("Hello,", input("What is your name? "), "Welcome to the Band Name Generator!")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your favorite pet?\n")
+print("The name of your band could be: " + city + " " + pet)
