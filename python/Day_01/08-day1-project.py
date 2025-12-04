af4c3b9aacddbbe26e71013effa97a993a5536c8
James Arong'o <git.drinkable312@passfwd.com>
2025-12-04 15:56:19 +0300

    Distinguishes text color for user input from questions using ANSI codes

--- python/Day_01/08-day1-project.py
+++ python/Day_01/08-day1-project.py
@@ -1,5 +1,8 @@
 # This a CLI program that creates a band name by combining answers from two questions
 print("Hello,", input("What is your name? "), "Welcome to the Band Name Generator!")
-city = input("Which city did you grow up in?\n")
-pet = input("What is the name of your favorite pet?\n")
-print("The name of your band could be: " + city + " " + pet)
+green = "\033[32m"
+reset = "\033[0m"
+city = input(green + "Which city did you grow up in?\n> ")
+print(reset + "What's your pet's name?")
+pet = input(green + "What is the name of your favorite pet?\n> ")
+print(reset + "The name of your band could be: " + city + " " + pet)
