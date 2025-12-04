119a1076ad7c716dce21caf22501a1d5ef3be463
James Arong'o <git.drinkable312@passfwd.com>
2025-12-04 13:40:10 +0300

    Calculates the length of an input string

--- python/Day_01/06-names.py
+++ python/Day_01/06-names.py
@@ -1,3 +1,3 @@
-# This program stores data in variables
-name = input("What is your name? ")
-print("Hello", name)
+# This program stores data in a variable and calculates its length using the len() function
+print(len(input("What is your name? ")))
+
