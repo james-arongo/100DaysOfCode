0b43bf320f3d172124de8fa0077c8d49a93bedd5
James Arong'o <git.drinkable312@passfwd.com>
2025-12-04 12:40:11 +0300

    Fixes bugs to print statements

--- python/Day_01/04-debug-practice.py
+++ python/Day_01/04-debug-practice.py
@@ -1,6 +1,7 @@
-# This program has bugs on all print statements that prevent successful execution of the program
-print ("Notes from Day 1.")
-print"The print statement is used to output strings.")
-print("Strings are strings of characters."
-print(String concatenation is done with the + sign")
-print"New lines can be crated with a \ and the letter n."
+# This program fixes the bugs that were on the print statememnts and now executes successfully.
+print("Notes from Day 1.")
+print("The print statement is used to output strings.")
+print("Strings are strings of characters.")
+print("String concatenation is done with the + sign.")
+print("New lines can be created with a \\ and the letter n.")
+
