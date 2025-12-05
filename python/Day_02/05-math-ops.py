d4cd0135337b839f2532b5d6faa22a4fc5cd4922
James Arong'o <git.drinkable312@passfwd.com>
2025-12-05 18:52:33 +0300

    Shows the basic math operations

---
+++ python/Day_02/05-math-ops.py
@@ -0,0 +1,9 @@
+# Math operations in Python.
+print("My age is: " + str(14))
+print(123 + 456)
+print(8 - 4)
+print(2 * 4)
+print(10 / 2)
+print(10 // 2) # Modulo operator - drops the decimal points and returns the remainder.
+print(5 // 2)
+print(2 ** 4) # Exponent
