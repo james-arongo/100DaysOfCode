438d6231a39e64f0ed6d6ca83cb79d7d06781d98
James Arong'o <git.drinkable312@passfwd.com>
2025-12-05 17:36:31 +0300

    Prints the primitive data types in Python

---
+++ python/Day_02/01-primitives.py
@@ -0,0 +1,13 @@
+# Adds two numbers
+print(123 + 456)
+
+# Large numbers. This is the same as '123456789'. The underscore separator gets ignored by the interpretor.
+print(123_456_789)
+
+#Float - Floatig point number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
