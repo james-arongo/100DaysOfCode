c8d84276bdb67c572875deaea343a7dd27f7e2fd
James Arong'o <git.drinkable312@passfwd.com>
2025-12-05 19:20:45 +0300

    Demonstrates precedence in order of operations between multiplication and division

---
+++ python/Day_02/06-order.py
@@ -0,0 +1,5 @@
+# Order of Operations
+# PEMDAS - Math operations in the same statement follow this order of operation (
+# Parentheses, Exponents, Multiplication, Division, Addition, Subtraction)
+print(4 * 4 + 4 / 4 - 4)
+print(4 / 4 + 4 * 4 - 4)
