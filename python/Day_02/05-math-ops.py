f0730c7dd3e3c351417553e49508cc5b9129de14
James Arong'o <git.drinkable312@passfwd.com>
2025-12-05 23:29:35 +0300

    Edit misleading comment

--- python/Day_02/05-math-ops.py
+++ python/Day_02/05-math-ops.py
@@ -4,6 +4,6 @@ print(123 + 456)
 print(8 - 4)
 print(2 * 4)
 print(10 / 2)
-print(10 // 2) # Modulo operator - drops the decimal points and returns the remainder.
+print(10 // 2) # Drops the decimal points and returns the remainder.
 print(5 // 2)
 print(2 ** 4) # Exponent
