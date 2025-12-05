52db46706da4779acf5c1d37bce1c4fcf1c6c27f
James Arong'o <git.drinkable312@passfwd.com>
2025-12-06 00:15:30 +0300

    Uses f-string to print data of different types

--- python/Day_02/09-assignment.py
+++ python/Day_02/09-assignment.py
@@ -1,9 +1,14 @@
-# This program demontrates use of asssignment operators and f-strings
+# This program demonstrates use of asssignment operators and f-strings
 
 # Assignment
 score = 0
+height = 1.8
+is_winning = True
 
 # User scores a point
 score += 1
 print(score)
+
+# f-strings
+print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}.")
 
